5ef7b65c4bbfe4555c7e64173f82cfc48be33bf0
Batzorigt Sanduijav <batzorigtbek@yahoo.com>
2021-08-28 14:44:31 -0400

    init commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("bata's project?");
+        // write your code here
+    }
+}
