1e5e8cdb20afd94747e87abe0c9383d8eda43787
Batzorigt Sanduijav <batzorigtbek@yahoo.com>
2021-08-29 09:37:33 -0400

    day2 class added

---
+++ src/com/company/GitDay2.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.sql.SQLOutput;
+
+public class GitDay2 {
+    public static void main(String[] args) {
+        System.out.println("Good morning !, its Git Day2");
+    }
+
+}
